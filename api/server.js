a21764caccafe3bfcee12cae6c8b865f1b39b8ea
tim-jeffries <timothyrexjeffries@gmail.com>
2020-02-23 20:15:05 -0800

    Answer self-study questions.

--- api/server.js
+++ api/server.js
@@ -2,15 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
-const authenticate = require('../auth/authenticate-middleware.js');
-const authRouter = require('../auth/auth-router.js');
-const jokesRouter = require('../jokes/jokes-router.js');
-
 const server = express();
-
-server.use(helmet());
-server.use(cors());
 server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+const authRouter = require('../auth/auth-router.js');
+const authenticate = require('../auth/authenticate-middleware.js');
+const jokesRouter = require('../jokes/jokes-router.js');
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
